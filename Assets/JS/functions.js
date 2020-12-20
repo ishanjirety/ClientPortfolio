f8156779b2ea41288bfb79ff37d6c38044d13bfc
ishanjirety <ishanjirety24@gmail.com>
2020-12-20 16:39:16 +0530

    style:Change Fonts And added shadows

--- Assets/JS/functions.js
+++ Assets/JS/functions.js
@@ -1,30 +1,8 @@
-function Show() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-    }
+$('.moreless-button').click(function() {
+  $('.moretext').slideToggle();
+  if ($('.moreless-button').text() == "Read more") {
+    $(this).text("Read less")
+  } else {
+    $(this).text("Read more")
   }
-
-  $(document).ready(function () {
-    $('.nav-toggle').click(function () {
-        var collapse_content_selector = $(this).attr('href');
-        var toggle_switch = $(this);
-        $(collapse_content_selector).toggle(function () {
-            if ($(this).css('display') == 'none') {
-                toggle_switch.html('Read More');
-            } else {
-                toggle_switch.html('Read Less');
-            }
-        });
-    });
-
 });
