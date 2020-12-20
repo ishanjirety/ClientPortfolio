8d204ffa09433e57fadeff2fc87b9b984a8e253a
ishanjirety <ishanjirety24@gmail.com>
2020-12-20 20:45:34 +0530

    style: added border animation

--- Assets/JS/functions.js
+++ Assets/JS/functions.js
@@ -1,8 +1,27 @@
-$('.moreless-button').click(function() {
-  $('.moretext').slideToggle();
-  if ($('.moreless-button').text() == "Read more") {
-    $(this).text("Read less")
-  } else {
-    $(this).text("Read more")
-  }
-});
+const ripple = el => {
+	let d = el.dataset.ripple.split('-'),
+		 s = 'rgba(3,149,229,0.4)',
+		 e = 'rgba(3,149,229,1)';
+	d[2] = Number(d[2]) + 4;
+	el.dataset.ripple = d.join('-');
+	el.style.background = `radial-gradient(circle at ${d[0]}px ${d[1]}px, ${s} 0%, ${s} ${d[2]}%, ${e} ${d[2] + 0.1}%)`;
+	
+	window.requestAnimationFrame(() => {
+		if (el.dataset.ripple && d[2] < 100) ripple(el)
+	});
+};
+
+const start = ev => {
+	ev.target.dataset.ripple = `${ev.offsetX}-${ev.offsetY}-0`
+	ripple(ev.target);
+};
+
+const stop = ev => {
+	let el = document.querySelector('[data-ripple]');
+	delete el.dataset.ripple;
+	el.style.background = 'none';
+};
+
+// Events
+document.querySelectorAll('.highlight').forEach( el => el.addEventListener( 'mousedown', start ));
+document.addEventListener( 'mouseup', stop );
